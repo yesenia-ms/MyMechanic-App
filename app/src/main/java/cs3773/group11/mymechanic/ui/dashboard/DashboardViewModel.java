4c028007c3c1f9ffa2c44e3df2cda6f354fff272
jesusgarcia <garjr3@gmail.com>
2024-02-29 16:20:52 -0600

    Project Created

---
+++ app/src/main/java/cs3773/group11/mymechanic/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package cs3773.group11.mymechanic.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/cs3773/group11/mymechanic/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package cs3773.group11.mymechanic.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
